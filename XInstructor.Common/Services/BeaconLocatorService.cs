2266892c60dd69758904590f320c982e25cc9c61
Romain DESSART <romaindessart@gmail.com>
2024-01-30 07:44:53 +0100

    Created Networking service

---
+++ XInstructor.Common/Services/BeaconLocatorService.cs
@@ -0,0 +1,20 @@
+﻿namespace XInstructor.Common.Services;
+
+public class BeaconLocatorService
+/// <summary> 
+/// Locate beacons calls (simulator) on broadcast port 50888
+/// Beacon lenght will be less than 500 bytes.
+/// Beacon structure : 
+///     * Author               = Aircraft Author
+///     * Description          = Aircraft Description
+///     * EmitPort             = Simulator Outbound Port
+///     * IPAddress            = Simulator IP Address
+///     * ListeningPort        = Simulator Inboudn Port
+///     * Operation            = Operation Type (should read 'beacon')
+///     * Simulator            = Simulator Familly ('X-Plane', 'P3D', 'MSFS', 'FS') 
+///     * SimulatorSDKVersion  = Simulator SDK Version
+///     * SimulatorVersion     = Simulator Version (X-Plane : 12090 for X-Plane 12.0.9.0)
+///     * Time                 = Beacon Emit Time (Simulator OS TIME when the beacon was emitted) 
+/// summary>
+{
+}
---
+++ XInstructor.Common/Services/UDPSimulatorService.cs
@@ -0,0 +1,10 @@
+﻿namespace XInstructor.Common.Services;
+
+public class UDPSimulatorService
+/// <summary>
+///     Send and receive data with simulator on ports :
+///         - 50555 (simulator inbound) 
+///         - 50556 (simulator outbound)
+/// </summary>
+{
+}
