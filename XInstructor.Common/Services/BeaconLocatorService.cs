3873af8589734292113ded570e916f67cbf26a69
Romain DESSART <romaindessart@gmail.com>
2024-01-30 10:03:25 +0100

    Beacon Locator Service

--- XInstructor.Common/Services/BeaconLocatorService.cs
+++ XInstructor.Common/Services/BeaconLocatorService.cs
@@ -1,4 +1,9 @@
-﻿namespace XInstructor.Common.Services;
+﻿using System.Diagnostics;
+using System.Net;
+using System.Net.Sockets;
+using System.Text;
+
+namespace XInstructor.Common.Services;
 
 public class BeaconLocatorService
 /// <summary> 
@@ -17,4 +22,51 @@ public class BeaconLocatorService
 ///     * Time                 = Beacon Emit Time (Simulator OS TIME when the beacon was emitted) 
 /// summary>
 {
+    private Socket _socket;
+    private IPEndPoint _bindEndPoint;
+    private EndPoint _remoteEP;
+    private byte[] _buffer;
+
+    //public event 
+
+    public BeaconLocatorService()
+    {
+        _remoteEP = new IPEndPoint(IPAddress.Any, 0);
+        _socket = new Socket(socketType: SocketType.Dgram, protocolType: ProtocolType.Udp, addressFamily: AddressFamily.InterNetwork);
+        _bindEndPoint = new IPEndPoint(IPAddress.Any, 0);
+        _buffer = new byte[500];
+    }
+
+    public bool Initalise(int targetPort = 50888)
+    {
+        _bindEndPoint.Port = targetPort;
+        try
+        {
+            _socket.Bind(_bindEndPoint);
+        }
+        catch(SocketException se)
+        {
+            Debug.WriteLine(se);
+            return false ;
+        }
+        return true;
+    }
+
+    public void Start() 
+    {
+        StartReceive();
+    }
+
+    private void StartReceive()
+    {
+        _socket.BeginReceiveFrom(_buffer, 0, 500, SocketFlags.None, ref _remoteEP, EndReceive, _socket);
+    }
+
+    private void EndReceive(IAsyncResult ar)
+    {
+        int received = (ar.AsyncState as Socket)!.EndReceiveFrom(ar, ref _remoteEP);
+        string data = Encoding.ASCII.GetString(_buffer, 0, received);
+        Debug.WriteLine($"Received {data}");
+        StartReceive();
+    }
 }
