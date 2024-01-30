ae459a81350b3e9bca72cb3417547fcbf7f72811
Romain DESSART <romaindessart@gmail.com>
2024-01-30 10:03:34 +0100

    Added console project tester

---
+++ XInstructor.Tester/Program.cs
@@ -0,0 +1,15 @@
+﻿using XInstructor.Common.Services;
+
+Console.WriteLine("X-INSTRUCTOR CMD TEST...");
+
+BeaconLocatorService beaconLocator = new BeaconLocatorService();
+if(!beaconLocator.Initalise(50888))
+{
+    Console.WriteLine($"Unable to initalise beacon locator");
+    return;
+}
+beaconLocator.Start();
+while (true)
+{
+    Thread.Sleep(100);
+}
